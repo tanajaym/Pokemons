ce58b9f73a7f715414eb5278cd4584e3ef98a195
tanajaym <evasy2002@gmail.com>
2025-01-15 19:03:57 +0300

    add: add axios and fetch data. change data later

--- tech-test-1/src/App.tsx
+++ tech-test-1/src/App.tsx
@@ -1,12 +1,96 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>h1</h1>
-    </div>
-  );
-}
+export const API_KEY = 'b76b0f17';
 
-export default App;
+export default function App() {
+
+    const [searchQuery, updateSearchQuery] = useState<string>("");
+    const [timeoutId, updateTimeoutId] = useState<NodeJS.Timeout>();
+    const [MovieList, updateMovieList] = useState<Movie[]>([]);
+    const [selectMovie, onMovieSelect] = useState<Movie | null>(null);
+
+    const fetchData = async (SearchStr: string) => {
+        const response = await axios.get(`git add`);
+        updateMovieList(response.data.Search);
+    };
+
+    const onTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        clearTimeout(timeoutId);
+        updateSearchQuery(event.target.value);
+        const timeout = setTimeout(() => fetchData(event.target.value), 500);
+        updateTimeoutId(timeout);
+    };
+
+    return (
+        <Container>
+            <Header>
+                <AppName>
+                    <MovieImg src="/movie_icon.svg"/>
+                    Evlix
+                </AppName>
+                <SrchBox>
+                    <SrchIcon src="/search_icon.svg"/>
+                    <SrchInput
+                        placeholder="Search Movie"
+                        value={searchQuery}
+                        onChange={onTextChange}
+                    />
+                </SrchBox>
+            </Header>
+            {selectMovie && <MovieInfComponent selectMovie={selectMovie}/>}
+            <MovieListContainer>
+                {MovieList?.length
+                    ? MovieList.map((movie, index) => (
+                        <MovieComponent
+                            key={index}
+                            movie={movie}
+                            onMovieSelect={onMovieSelect}
+                        />
+                    ))
+                    : "No movie search"}
+            </MovieListContainer>
+        </Container>
+    );
+};
+
+
+// const { observer, useLocalObservable } = mobxReactLite;
+// const { useEffect } = React;
+//
+// const App = observer(function App() {
+//     const store = useLocalObservable(() => ({
+//         category: null,
+//         setCategory(category) {
+//             store.category = category;
+//         }
+//     }));
+//
+//     useEffect(() => {
+//         fetch("https://jservice.io/api/category?id=150")
+//             .then((res) => res.json())
+//             .then((res) => store.setCategory(res));
+//     }, []);
+//
+//     if (store.category === null) return null;
+//
+//     return (
+//         <div>
+//             <div>{store.category.title}</div>
+//             {store.category.clues.map((clue) => (
+//                 <div key={clue.id}>{clue.question}</div>
+//             ))}
+//         </div>
+//     );
+// });
+//
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/react/17.0.1/umd/react.production.min.js"></script>
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/react-dom/17.0.1/umd/react-dom.production.min.js"></script>
+// <script src="https://cdn.jsdelivr.net/npm/mobx@6.0.4/dist/mobx.umd.production.min.js"></script>
+// <script src="https://cdn.jsdelivr.net/npm/mobx-react-lite@3.1.6/dist/mobxreactlite.umd.production.min.js"></script>
+// <script src="https://cdn.jsdelivr.net/npm/mobx-react@7.0.5/dist/mobxreact.umd.production.min.js"></script>
+//
+// <div id="root"></div>
---
+++ tech-test-1/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'none',
+    entry: {
+        app: path.join(__dirname, 'src', 'index.tsx')
+    },
+    target: 'web',
+    resolve: {
+        extensions: [".js", ".json", ".ts", ".tsx"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+                exclude: '/node_modules/'
+            }
+        ],
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    }
+}
